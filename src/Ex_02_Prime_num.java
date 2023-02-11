61e41564b72e2fb4612d8cdc13b4e2c2680de8e8
Mayya <msalosina@mail.ru>
2023-02-11 12:52:02 +0700

    Ex_02 Prime numbers

--- src/Ex_02_Prime_num.java
+++ src/Ex_02_Prime_num.java
@@ -1,2 +1,19 @@
+// 2. Вывести все простые числа от 1 до 1000
+import java.util.Scanner;
 public class Ex_02_Prime_num {
-}
+
+    public static void main(String[] args) {
+        boolean b = true;
+            for (int prime = 2; prime <= 1000; prime++){
+                for (int i = 2; i < prime; i++){
+                    if (prime % i == 0){
+                        b = false;
+                        break;
+                    }
+                }
+                if (b) System.out.printf("%d ", prime);
+                else b = true;
+            }
+        }
+    }
+
